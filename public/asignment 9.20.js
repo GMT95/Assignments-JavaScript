6adce1d6fb094b74e6532b8835c50072c72b8ea9
GMT95 <gmtalha9@gmail.com>
2018-03-06 20:51:04 +0500

    3

--- public/asignment 9.20.js
+++
@@ -1,7 +0,0 @@
-function caps(Str) {
-    var newStr = "";
-    for(i = 0;i <= Str.length - 1; i++) {
-        newStr = (Str[0]).toUpperCase();
-    }
-    return newStr;
-}
---
+++ public/assignment 9.20.js
@@ -0,0 +1,3 @@
+function caps(str) {
+    
+}
